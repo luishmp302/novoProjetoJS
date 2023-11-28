4fca6f82cf12e948bd9f544f16cc070ad93a1a6b
LuisHMP0 <148148975+LuisHMP0@users.noreply.github.com>
2023-11-27 23:21:04 -0300

    commit01

---
+++ script.js
@@ -0,0 +1,30 @@
+function showPage(pageId) {
+    document.getElementById('home').style.display = 'none';
+    document.getElementById('contato').style.display = 'none';
+    document.getElementById('areaAdmin').style.display = 'none';
+
+    document.getElementById(pageId).style.display = 'block';
+}
+
+function sendMessage() {
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var msg = document.getElementById('msgEnviar').value;
+    var message = name + '( ' + email + ')' + ': ' + msg;
+
+    var messagesDiv = document.getElementById('messages');
+    messagesDiv.innerHTML += '<p>' + message + '</p>';
+}
+
+function adminLogin() {
+    var username = document.getElementById('adminUsername').value;
+    var password = document.getElementById('adminPassword').value;
+
+    if (username === 'admin' && password === 'admin') {
+        // Exibir mensagens recebidas na área admin
+        var adminMessagesDiv = document.getElementById('adminMessages');
+        adminMessagesDiv.innerHTML = document.getElementById('messages').innerHTML;
+    } else {
+        alert('Login inválido. Tente novamente.');
+    }
+}
